cbb34f05cc9a7b6425fc83d8f3a12e0220a5d824
Admin <y0985160s@iespoblenou.org>
2020-12-18 19:48:25 +0100

    Commit inicial

---
+++ src/sample/Controller.java
@@ -0,0 +1,75 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    @FXML
+    private MenuBar mBMenue;
+    @FXML
+    private Menu menueFitxer;
+    @FXML
+    private MenuItem menueItemSortir;
+    @FXML
+    private Menu menueEditar;
+    @FXML
+    private MenuItem menueItemCopiar;
+    @FXML
+    private MenuItem menueItemTallar;
+    @FXML
+    private MenuItem menueItemEngaxar;
+    @FXML
+    private MenuItem menueItemDesfer;
+    @FXML
+    private Menu menueOpcions;
+    @FXML
+    private MenuItem menueItemFont;
+    @FXML
+    private MenuItem menueItemTamany;
+    @FXML
+    private Menu menueAjuda;
+    @FXML
+    private MenuItem menueItemSobreEditor;
+    @FXML
+    private TextArea taText;
+
+
+    @FXML
+    void onChangeFont() {
+
+    }
+
+    @FXML
+    void onChangeSize() {
+
+    }
+
+    @FXML
+    void onCopy() {
+
+    }
+
+    @FXML
+    void onCut() {
+
+    }
+
+    @FXML
+    void onExit() {
+
+    }
+
+    @FXML
+    void onPaste() {
+
+    }
+
+    @FXML
+    void onUndo() {
+
+    }
+
+}
---
+++ src/sample/TextFile.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.scene.shape.Path;
+
+import java.util.List;
+
+public class TextFile {
+
+    private final Path file;
+    private final List<String> content;
+
+    public TextFile(Path file, List<String> content) {
+        this.file = file;
+        this.content = content;
+    }
+
+    public Path getFile() {
+        return file;
+    }
+
+    public List<String> getContent() {
+        return content;
+    }
+}
